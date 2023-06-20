0e7f71ab488d575e3ea3fe427484b633c8afe790
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2023-06-20 23:25:16 +0200

    added Tilt

--- src/pages/FindDogs.tsx
+++ src/pages/FindDogs.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { RotatingLines } from 'react-loader-spinner';
+import { Tilt } from 'react-tilt';
+
 import { getDogsByBreed } from '../API/api-service';
+
 import { SearchBar } from '../Components';
 
 type StatusOptions = {
@@ -8,6 +11,18 @@ type StatusOptions = {
   PENDING: string;
   RESOLVED: string;
   REJECTED: string;
+};
+
+const defaultOptions = {
+  reverse: false, // reverse the tilt direction
+  max: 35, // max tilt rotation (degrees)
+  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 1000, // Speed of the enter/exit transition
+  transition: true, // Set a transition on enter/exit.
+  axis: null, // What axis should be disabled. Can be X or Y.
+  reset: true, // If the tilt effect has to be reset on exit.
+  easing: 'cubic-bezier(.03,.98,.52,.99)', // Easing on enter/exit.
 };
 
 const statusOptions: StatusOptions = {
@@ -69,6 +84,12 @@ export const FindDogs = () => {
           />
         </div>
       )}
+      <Tilt
+        options={defaultOptions}
+        style={{ height: 250, width: 250, backgroundColor: 'tomato' }}
+      >
+        <p>s</p>
+      </Tilt>
     </div>
   );
 };
