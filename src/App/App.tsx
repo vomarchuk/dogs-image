ae6c3799212715678bf51a696579770cddb0d0eb
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2023-06-23 00:52:27 +0200

    fixed package

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -11,7 +11,7 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/find-dogs" element={<FindDogs />} />
+        <Route path="/find" element={<FindDogs />} />
       </Routes>
       <Footer />
       <ToastContainer autoClose={3000} />
--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className="container fixed left-[50%] bottom-0 translate-x-[-50%] py-3 text-center bg-blue-300 ">
       <Button href="/" name="Home" />
-      <Button href="/find-dogs" name="Find dogs" />
+      <Button href="/find" name="Find dogs" />
     </footer>
   );
 };
